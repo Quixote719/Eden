51038f5a735689cc138fbb1246654cd54ae62676
Quixote719 <icarus7195@163.com>
2023-01-11 15:46:22 +0800

    uninstall tailwind

--- src/pages/earth.tsx
+++ src/pages/earth.tsx
@@ -4,9 +4,7 @@ import styles from './index.module.less';
 const Earth = () => {
   return (
     <div>
-      <div className="flex items-center bg-gradient-to-br rounded-xl shadow-md p-6 space-x-4 max-w-sm mx-auto">
-        Earth
-      </div>
+      <div className={styles.pageTitle}>Earth</div>
     </div>
   );
 };
--- src/pages/venus.tsx
+++ src/pages/venus.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.less';
 const Venus = () => {
   return (
     <div className={styles.pageContainer}>
-      <div className="w-64 h-8 bg-gradient-to-br from-fuchsia-500 to-purple-600">Venus</div>
+      <div>Venus</div>
     </div>
   );
 };
