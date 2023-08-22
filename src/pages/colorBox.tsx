af20bb53eed5d9b88149e373bf9dc7df5fe8bcf6
Daniel <lwang41@jaguarlandrover.com>
2023-08-22 11:24:12 +0800

    1

--- src/pages/colorBox.tsx
+++ src/pages/colorBox.tsx
@@ -8,7 +8,13 @@ const colorMap = {
   "#f1faee" : 'Winter Oasis',
   "#a8dadc" : 'Crystal',
   "#457b9d" : 'Linoleum Blue',
-  "#1d3557" : 'Elite Blue'
+  "#1d3557" : 'Elite Blue',
+
+  "0081A7" : 'Celadon Blue',
+  "00AFB9" : 'Tiffany Blue',
+  "FDFCDC" : 'Cornsilk',
+  "FED9B7" : "Sandy Tan",
+  "F07167" : 'Bittersweet'
 }
 
 const Jupiter = () => {
@@ -23,14 +29,13 @@ const Jupiter = () => {
   }
 
   const getTextColor = (item) => {
+    console.log(111, item)
     let colorArr = [], len = item.length, colorHex = parseHex(item)
-    console.log('colorHex', colorHex)
     colorArr.push(colorHex.slice(0,2))
     colorArr.push(colorHex.slice(2,4))
     colorArr.push(colorHex.slice(4))
     let colorLine = colorArr.map(item => parseInt(item, 16)).reduce((prev, cur) => prev + cur)
-    console.log('colorLine', colorLine)
-    let res = ((colorLine / 3) > (255 / 2)) ? '#121212' : '#EDEDED'
+    let res = ((colorLine / 3) > (255 / 2)) ? '#101010' : '#EFEFEF'
     return res
   }
 
@@ -39,9 +44,9 @@ const Jupiter = () => {
       <button onClick={()=>downloadCardImage()} className={styles.downloadBtn} style={{margin: '10px', display: 'block'}}>download the card image</button>
       <div className={styles.cardDiv} id='mythCard'>
         {
-            colorPallete(7).map(item => 
+            colorPallete(6).map(item => 
                 <div key={item} className={styles.colorPaper} style={{background: item}}>
-                    <div style={{ color: getTextColor(item) }} className={styles.colorEn}>{colorMap[item]}</div>
+                    {/* <div style={{ color: getTextColor(item) }} className={styles.colorEn}>{colorMap[parseHex(item)]}</div> */}
                     <div style={{ color: getTextColor(item) }} className={styles.colorHex}>{parseHex(item)}</div>
                 </div>
             )
