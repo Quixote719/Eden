1ba7e8ef0db4b18c0abb8e54e281cbcbbf89d483
Daniel <lwang41@jaguarlandrover.com>
2024-01-09 17:48:29 +0800

    vitest test

--- src/tests/util.test.js
+++ src/tests/util.test.js
@@ -4,3 +4,7 @@ import { sumUp } from '../utils/algo';
 test('result shoud be 21', () => {
   expect(sumUp(1, 2, 3, 4, 5, 6)).toBe(21);
 });
+
+test('result shoud be 100', () => {
+  expect(sumUp(30, 23, 47)).toBe(100);
+});
